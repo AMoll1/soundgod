23fbb8f164b979d6bec881886f94b0a471533ec1
moll.alexander@gmail.com <braunbiagsmiher420!!>
2020-06-15 18:37:41 +0200

    avglist ??

--- lib/main.dart
+++ lib/main.dart
@@ -86,8 +86,6 @@ class _HomeMeasurementState extends State<HomeMeasurement> {
     tempMin = 0;
     if (Platform.isIOS) {
       controller = new AudioController(CommonFormat.Int16, 44100, 1, true);
-      avgList = List<double>();
-
     }
   }
 
