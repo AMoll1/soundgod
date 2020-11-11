b667d0ccf3a971544cffb3515acea0f7cc46826a
moll.alexander@gmail.com <braunbiagsmiher420!!>
2020-11-11 13:29:24 +0100

    speichern anfang

--- lib/settings.dart
+++ lib/settings.dart
@@ -28,6 +28,7 @@ class _SettingsMeasurementState extends State<SettingsScreen> {
     calib3  = prefs.getInt('calib3')?? 0;
     calib4  = prefs.getInt('calib4')?? 0;
     calib5  = prefs.getInt('calib5')?? 0;
+    setState(() {});
   }
 
   setValues() async {
