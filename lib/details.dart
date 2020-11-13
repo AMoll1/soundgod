5f0db7e032535dfb8819a8cb970474a9a76e4363
FT <edutilflo001@fh-kaernten.at>
2020-11-13 18:25:49 +0100

    detail anpassung

--- lib/details.dart
+++ lib/details.dart
@@ -14,6 +14,12 @@ class DetailView extends StatelessWidget {
   DetailView({Key key, @required this.measurement}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
+    TextStyle stl = new TextStyle(
+      fontSize: 14.0,
+      color: Colors.green,
+    );
+
     return AlertDialog(
       backgroundColor: Colors.grey[800],
       /*title: Text(
@@ -32,10 +38,298 @@ class DetailView extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             //mainAxisSize: MainAxisSize.max,
-            //crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              _buildDetailView(),
-              //_buildLogoAttribution(),
+              Card(
+                color: Colors.grey[850],
+                child: Text(
+                  'Measurement Data',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text('Weighting:', style: stl),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.weighting,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  )
+                ],
+              ),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Text('DateTime:', style: stl),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      dateFormat
+                          .format(DateTime.tryParse(measurement.dateTime)),
+                      style: stl,
+                    ),
+                    flex: 4,
+                  )
+                ],
+              ),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Sound min:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      txtFormat.format(measurement.soundMin).toString() + " dB",
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Sound max:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      txtFormat.format(measurement.soundMax).toString() + " dB",
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Sound avg:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      txtFormat.format(measurement.soundAvg).toString() + " dB",
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Sound duration:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.soundDuration.toString() + " s",
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              Card(
+                color: Colors.grey[850],
+                child: Text(
+                  'Location Data',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Latitude:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.latitude.toString(),
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Longitude:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.longitude.toString(),
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Address:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.address,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Card(
+                color: Colors.grey[850],
+                child: Text(
+                  'Device Data',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'ID Device:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.idDevice,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Manufacturer:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.manufacturer,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Model:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.model,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      Platform.isAndroid ? 'SDK Version:' : 'OS Version:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      Platform.isAndroid
+                          ? measurement.sdkVersion
+                          : measurement.osVersion,
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      measurement.isPhysicalDevice == 1
+                          ? 'PhysicalDevice:'
+                          : 'PhysicalDevice:',
+                      style: stl,
+                    ),
+                    flex: 3,
+                  ),
+                  Expanded(
+                    child: Text(
+                      measurement.isPhysicalDevice == 1 ? 'TRUE' : 'FALSE',
+                      style: stl,
+                    ),
+                    flex: 4,
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -48,331 +342,6 @@ class DetailView extends StatelessWidget {
           },
           textColor: Colors.green,
           child: const Text('close'),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDetailView() {
-    TextStyle stl = new TextStyle(
-      fontSize: 15.0,
-      color: Colors.green,
-    );
-    return Column(
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            //SizedBox(height: 5.0),
-            Card(
-              color: Colors.grey[850],
-              child: Text(
-                'Measurement Data',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-            SizedBox(height: 5.0),
-
-            Row(
-              children: [
-                Expanded(
-                  child: Text('Weighting:', style: stl),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.weighting.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                )
-              ],
-            ),
-
-            Row(
-              children: [
-                Expanded(
-                  child: Text('DateTime:', style: stl),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    dateFormat
-                        .format(DateTime.tryParse(measurement.dateTime))
-                        .toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                )
-              ],
-            ),
-
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Sound min:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    txtFormat.format(measurement.soundMin).toString() + " dB",
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Sound max:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    txtFormat.format(measurement.soundMax).toString() + " dB",
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Sound avg:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    txtFormat.format(measurement.soundAvg).toString() + " dB",
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Sound duration:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.soundDuration.toString() + " s",
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-
-          ],
-        ),
-        SizedBox(height: 5.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              color: Colors.grey[850],
-              child: Text(
-                'Location Data',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-            SizedBox(height: 5.0),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Latitude:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.latitude.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Longitude:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.longitude.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Address:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.address.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Card(
-              color: Colors.grey[850],
-              child: Text(
-                'Device Data',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-            SizedBox(height: 5.0),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'ID Device:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.idDevice.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Manufacturer:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.manufacturer.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Model:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.model.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    Platform.isAndroid ? 'SDK Version:' : 'OS Version:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    Platform.isAndroid
-                        ? measurement.sdkVersion.toString()
-                        : measurement.osVersion.toString(),
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    measurement.isPhysicalDevice == 1
-                        ? 'PhysicalDevice:'
-                        : 'PhysicalDevice:',
-                    style: stl,
-                  ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Text(
-                    measurement.isPhysicalDevice == 1 ? 'TRUE' : 'FALSE',
-                    style: stl,
-                  ),
-                  flex: 3,
-                ),
-              ],
-            ),
-
-            /*Text(
-              'sdk Version: ' + measurement.sdkVersion,
-              style: stl,
-            ),*/
-          ],
         ),
       ],
     );
